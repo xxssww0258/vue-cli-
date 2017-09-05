9da06831a4d8832ba4b6c632ea3012c6593f52ea
John <xxssww0258@sina.com>
2017-09-05 21:50:37 +0800

    翻译

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,5 +2,5 @@ var merge = require('webpack-merge')//引入webpack-merge
 var prodEnv = require('./prod.env')//{NODE_ENV: '"production"'}
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"'//NODE_ENV 加冒号是为了兼容某些插件 避免使用了JSON.parse()生成了函数而不是字符串
 })//合并之后还是{NODE_ENV: '"production"'}
